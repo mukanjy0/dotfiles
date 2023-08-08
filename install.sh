47eadced89edccf1b54baa0fa021aaf47926c55f
mukanjyo <mukanjyo29@protonmail.com>
2023-08-08 18:13:03 -0500

    add ZSH config

--- install.sh
+++ install.sh
@@ -15,3 +15,9 @@ ln -s "$HOME/dotfiles/X11" "$HOME/.config/X11"
 ######
 rm -rf "$HOME/.config/i3"
 ln -s "$HOME/dotfiles/i3" "$HOME/.config/i3"
+#######
+# ZSH #
+#######
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
+ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
